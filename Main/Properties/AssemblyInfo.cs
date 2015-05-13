fdcda3531569f9e8f0595506a2a41e283313b264
Nestor Soriano Vilchez <nestor.soriano@sunhotels.net>
2015-05-13 09:14:41 +0200

    Solution created with the Main project and a reference to Z80.NET.

---
+++ Main/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Main")]
+[assembly: AssemblyDescription("A toolbox to ease unit testing of Z80 code together with Z80.NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sunhotels")]
+[assembly: AssemblyProduct("Main")]
+[assembly: AssemblyCopyright("Copyright © Konamiman 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("40f0f95e-2f99-441a-86dd-f12923bfab66")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
